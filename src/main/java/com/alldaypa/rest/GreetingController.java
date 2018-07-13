87b6de16c12c2b30bceea87e5ea313bfb6757ca6
Andy <andy.wong@alldaypa.com>
2018-07-13 12:37:47 +0100

    1st fixed pom.xml and added lombok logging

--- src/main/java/com/alldaypa/rest/GreetingController.java
+++ src/main/java/com/alldaypa/rest/GreetingController.java
@@ -1,10 +1,13 @@
 package com.alldaypa.rest;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class GreetingController {
 
@@ -15,6 +18,7 @@ public class GreetingController {
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
 
+        log.info("I'm in greeting()");
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
@@ -23,12 +27,14 @@ public class GreetingController {
     @RequestMapping("/hello")
     public String hello(@RequestParam(value="name") String name) {
 
+        log.info("I'm in hello()");
         return "Hello " + name;
     }
 
     // http://localhost:8080/
     @RequestMapping("/")
     public String index() {
+        log.info("I'm in index()");
         return "Hello";
     }
 }
